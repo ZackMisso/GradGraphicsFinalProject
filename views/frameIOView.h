8d232b897295fafe21fcb71c2db29ff0c6f35179
Benjamin Lin <benlin@cs.utexas.edu>
2016-05-08 11:40:11 -0500

    might have done bmp snapshot

--- views/frameIOView.h
+++ views/frameIOView.h
@@ -4,6 +4,7 @@
 // This View is used to test Input and Output of the current Frame
 #define GLFW_INCLUDE_GLU
 #include <GLFW/glfw3.h>
+#include "../io/bitmap.h"
 #include "view.h"
 
 class FrameIOView : public View {
